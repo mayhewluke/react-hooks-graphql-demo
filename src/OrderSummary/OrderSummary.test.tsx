c447d3c14d993625ba8c9fd7445a0d47d996e840
Luke Mayhew <luke@webuildlegends.com>
2019-03-27 17:44:41 -0600

    Add tests for OrderSummary

---
+++ src/OrderSummary/OrderSummary.test.tsx
@@ -0,0 +1,62 @@
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+import React from "react";
+import { MockedProvider, MockedResponse } from "react-apollo/test-utils";
+import { render as rtlRender, wait } from "react-testing-library";
+
+import LineItem from "../LineItem/LineItem";
+import OrderSummary, { SIZES_QUERY } from "./OrderSummary";
+
+jest.mock("../LineItem/LineItem", () => jest.fn(() => false));
+
+describe("OrderSummary", () => {
+  // tslint:disable-next-line:only-arrow-functions
+  const render = (response: Partial<MockedResponse>) =>
+    rtlRender(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: SIZES_QUERY,
+            },
+            ...response,
+          },
+        ]}
+        addTypename={false}
+      >
+        <OrderSummary />
+      </MockedProvider>,
+    );
+  const pizzaSizes = [
+    { name: "mini", basePrice: 1 },
+    { name: "jumbo", basePrice: 2 },
+  ];
+
+  it("shows a loading message while pizza info is loading", () => {
+    const { container } = render({});
+    expect(container).toHaveTextContent(/loading/i);
+  });
+
+  it("shows an error message if the network fails", async () => {
+    const { container } = render({ error: new Error("Something went wrong!") });
+    await wait(() => expect(container).toHaveTextContent(/error/i));
+  });
+
+  it("shows an error message if the query fails", async () => {
+    const { container } = render({
+      result: { errors: [{ message: "Error!" }] },
+    });
+    await wait(() => expect(container).toHaveTextContent(/error/i));
+  });
+
+  // TODO replace with *actual* order info
+  it("fake populates the order with the loaded pizza sizes", () => {
+    render({ result: { data: { pizzaSizes } } });
+    wait(() =>
+      pizzaSizes.map(({ name, basePrice }) =>
+        expect(LineItem).toHaveBeenCalledWith({ size: name, cost: basePrice }),
+      ),
+    );
+  });
+});
--- src/OrderSummary/OrderSummary.tsx
+++ src/OrderSummary/OrderSummary.tsx
@@ -4,7 +4,7 @@ import { Query } from "react-apollo";
 
 import LineItem from "../LineItem/LineItem";
 
-const sizesQuery = gql`
+export const SIZES_QUERY = gql`
   {
     pizzaSizes {
       name
@@ -14,29 +14,31 @@ const sizesQuery = gql`
 `;
 
 interface SizesQueryData {
-  pizzaSizes: Array<{ name: string; maxToppings: number; basePrice: number }>;
+  pizzaSizes: Array<{ name: string; basePrice: number }>;
 }
 
 const OrderSummary = () => (
-  <Query<SizesQueryData> query={sizesQuery}>
+  <Query<SizesQueryData> query={SIZES_QUERY}>
     {({ loading, error, data }) => {
       if (loading) return <p>Loading...</p>;
       if (error) return <p>{error.toString()}</p>;
       return (
-        <table id="order-summary">
-          <thead>
-            <tr>
-              <th>Size</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data &&
-              data.pizzaSizes.map(({ name, basePrice }) => (
-                <LineItem key={name} size={name} cost={basePrice} />
-              ))}
-          </tbody>
-        </table>
+        <>
+          <table id="order-summary">
+            <thead>
+              <tr>
+                <th>Size</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data &&
+                data.pizzaSizes.map(({ name, basePrice }) => (
+                  <LineItem key={name} size={name} cost={basePrice} />
+                ))}
+            </tbody>
+          </table>
+        </>
       );
     }}
   </Query>
