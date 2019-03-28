4d61cc7e84e105054e60d32337f20c78cd1d55af
Luke Mayhew <luke@webuildlegends.com>
2019-03-28 13:04:44 -0600

    AddPizza displays prices and fetches toppings + topping prices

--- src/AddPizza/AddPizza.test.tsx
+++ src/AddPizza/AddPizza.test.tsx
@@ -1,24 +1,155 @@
 import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
 
-import { getByText as domGetByText } from "dom-testing-library";
 import React from "react";
-import { render as rtlRender, waitForElement } from "react-testing-library";
+import { MockedProvider, MockedResponse } from "react-apollo/test-utils";
+import {
+  fireEvent,
+  render as rtlRender,
+  wait,
+  waitForElement,
+  within,
+} from "react-testing-library";
 
-import AddPizza from "../AddPizza/AddPizza";
+import AddPizza, { TOPPINGS_QUERY } from "../AddPizza/AddPizza";
 
 describe("AddPizza", () => {
-  const pizzaSizes = [{ name: "mini" }, { name: "jumbo" }];
-  const render = () => rtlRender(<AddPizza pizzaSizes={pizzaSizes} />);
+  const pizzaSizes = [
+    { name: "mini", basePrice: 10 },
+    { name: "jumbo", basePrice: 20 },
+  ];
+  const request = (pizzaSizeIndex: number = 0) => ({
+    query: TOPPINGS_QUERY,
+    variables: { pizzaSize: pizzaSizes[pizzaSizeIndex].name.toUpperCase() },
+  });
+  const data = {
+    pizzaSizeByName: {
+      toppings: [
+        {
+          defaultSelected: true,
+          topping: {
+            name: "Pepperoni",
+            price: 1,
+          },
+        },
+        {
+          defaultSelected: false,
+          topping: {
+            name: "Cheese",
+            price: 2,
+          },
+        },
+      ],
+    },
+  };
+  const mockGql = (pizzaSizeIndex: number = 0) => ({
+    request: request(pizzaSizeIndex),
+    result: { data },
+  });
+  const render = (mocks: MockedResponse[]) =>
+    rtlRender(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <AddPizza pizzaSizes={pizzaSizes} />
+      </MockedProvider>,
+    );
 
   it("allows any size of pizza to be selected", async () => {
-    const { getByLabelText, container } = render();
+    const { getByLabelText, container } = render([]);
     const sizeSelect = await waitForElement(() => getByLabelText("Size"), {
       container,
     });
 
     pizzaSizes.map(({ name }) =>
-      expect(domGetByText(sizeSelect, name)).toHaveAttribute("value", name),
+      expect(
+        within(sizeSelect).getByText(name, { exact: false }),
+      ).toHaveAttribute("value", name),
     );
+  });
+
+  it("displays pizza prices in the dropdown", async () => {
+    const { getByLabelText, container } = render([]);
+    const sizeSelect = await waitForElement(() => getByLabelText("Size"), {
+      container,
+    });
+
+    pizzaSizes.map(({ name, basePrice }) =>
+      expect(
+        within(sizeSelect).getByText(name, { exact: false }),
+      ).toHaveTextContent(`$${basePrice}`),
+    );
+  });
+
+  it("selects the first pizza size by default", async () => {
+    const { getByLabelText } = render([]);
+    const sizeSelect = (await waitForElement(() =>
+      getByLabelText("Size"),
+    )) as HTMLInputElement;
+
+    expect(sizeSelect.value).toEqual(pizzaSizes[0].name);
+  });
+
+  describe("toppings", () => {
+    it("automatically loads and displays the toppings for the first pizza", () => {
+      const { toppings } = data.pizzaSizeByName;
+      const { getByLabelText } = render([mockGql()]);
+
+      return Promise.all(
+        toppings.map(({ topping: { name, price } }) =>
+          wait(() =>
+            // Check that the label contains both the topping name and the price
+            expect(
+              (getByLabelText(name, { exact: false }) as HTMLInputElement)
+                .labels![0],
+            ).toHaveTextContent(`$${price}`),
+          ),
+        ),
+      );
+    });
+
+    it("shows a loading message when the toppings are loading", () => {
+      const { container } = render([mockGql()]);
+      expect(container).toHaveTextContent(/loading/i);
+    });
+
+    it("shows an error message when the toppings fail to load", async () => {
+      const { container } = render([
+        {
+          error: new Error("Something went wrong!"),
+          request: request(),
+        },
+      ]);
+      await wait(() => expect(container).toHaveTextContent(/error/i));
+    });
+
+    it("preselects toppings according to their defaults", async () => {
+      const selectedTopping = data.pizzaSizeByName.toppings[0].topping.name;
+      const unselectedTopping = data.pizzaSizeByName.toppings[1].topping.name;
+      const { getByLabelText } = render([mockGql()]);
+
+      const selectedIsChecked = ((await waitForElement(() =>
+        getByLabelText(selectedTopping, { exact: false }),
+      )) as HTMLInputElement).checked;
+      const unselectedIsChecked = ((await waitForElement(() =>
+        getByLabelText(unselectedTopping, { exact: false }),
+      )) as HTMLInputElement).checked;
+
+      expect(selectedIsChecked).toBe(true);
+      expect(unselectedIsChecked).toBe(false);
+    });
+
+    it("loads toppings when the pizza size is changed", async () => {
+      const size2 = pizzaSizes[1].name;
+      const size2Topping = data.pizzaSizeByName.toppings[0].topping.name;
+      const { getByLabelText } = render([
+        // First request returns no toppings to avoid false positives
+        { request: request(0), result: { data: {} } },
+        mockGql(1),
+      ]);
+
+      const sizeSelect = await waitForElement(() => getByLabelText(/size/i));
+      fireEvent.change(sizeSelect, { target: { value: size2 } });
+
+      await wait(() => getByLabelText(size2Topping, { exact: false }));
+    });
   });
 });
--- src/AddPizza/AddPizza.tsx
+++ src/AddPizza/AddPizza.tsx
@@ -1,23 +1,98 @@
-import React from "react";
+import gql from "graphql-tag";
+import React, { useCallback, useState } from "react";
+import { Query } from "react-apollo";
 
-interface Props {
-  pizzaSizes: Array<{ name: string }>;
+export const TOPPINGS_QUERY = gql`
+  query Toppings($pizzaSize: PizzaSizes!) {
+    pizzaSizeByName(name: $pizzaSize) {
+      toppings {
+        topping {
+          name
+          price
+        }
+        defaultSelected
+      }
+    }
+  }
+`;
+
+interface Topping {
+  name: string;
+  price: number;
 }
 
-const AddPizza: React.SFC<Props> = ({ pizzaSizes }) => (
-  <div>
-    <h3>Add a new pizza</h3>
-    <form>
-      <label htmlFor="size">Size</label>
-      <select id="size">
-        {pizzaSizes.map(({ name }) => (
-          <option key={name} value={name}>
-            {name}
-          </option>
-        ))}
-      </select>
-    </form>
-  </div>
-);
+interface ToppingsData {
+  pizzaSizeByName: {
+    toppings: Array<{ topping: Topping; defaultSelected: boolean }>;
+  };
+}
+
+interface Props {
+  pizzaSizes: Array<{ name: string; basePrice: number }>;
+}
+
+const AddPizza: React.SFC<Props> = ({ pizzaSizes }) => {
+  const [size, setSize] = useState(pizzaSizes[0].name);
+  const [toppingSelections, setToppingSelections] = useState({} as Record<
+    string,
+    boolean
+  >);
+  const toggleTopping = useCallback(
+    (name: string) =>
+      setToppingSelections({
+        ...toppingSelections,
+        [name]: !toppingSelections[name],
+      }),
+    [toppingSelections],
+  );
+  return (
+    <Query<ToppingsData>
+      query={TOPPINGS_QUERY}
+      variables={{ pizzaSize: size.toUpperCase() }}
+    >
+      {({ loading, error, data }) => (
+        <div>
+          <h3>Add a new pizza</h3>
+          <form>
+            <label htmlFor="size">Size</label>
+            <select
+              id="size"
+              value={size}
+              onChange={e => setSize(e.target.value)}
+            >
+              {pizzaSizes.map(({ name, basePrice }) => (
+                <option key={name} value={name}>
+                  {name} - ${basePrice}
+                </option>
+              ))}
+            </select>
+            <div id="toppings">
+              {loading && <p>Loading toppings...</p>}
+              {error && <p>{error.toString()}</p>}
+              {data &&
+                data.pizzaSizeByName &&
+                data.pizzaSizeByName.toppings.map(
+                  ({ topping: { name, price }, defaultSelected }) => (
+                    <div key={name} className="topping">
+                      <label htmlFor={name}>
+                        {name}: ${price}
+                      </label>
+                      <input
+                        id={name}
+                        name={name}
+                        type="checkbox"
+                        checked={toppingSelections[name] || defaultSelected}
+                        onChange={() => toggleTopping(name)}
+                      />
+                    </div>
+                  ),
+                )}
+            </div>
+          </form>
+        </div>
+      )}
+    </Query>
+  );
+};
 
 export default AddPizza;
